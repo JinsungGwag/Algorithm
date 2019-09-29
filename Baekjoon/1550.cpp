7cb65afea12b805d3c7de39b7e1ab1cee28a2135
Jinsung Gwag <jspartner1@naver.com>
2019-09-29 16:21:12 +0900

    Upload solution of 1550

---
+++ Baekjoon/1550.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <math.h>
+
+int main()
+{
+	char num16[7];
+	scanf("%s", num16);
+
+	int size = 0;
+	for(int i = 0; num16[i] != '\0'; i++)
+		size++;
+	
+	int total = 0;
+	for(int i = 0; i < size; i++)
+	{
+		if((int)num16[i] <= 70 && (int)num16[i] >= 65)
+			total += ((int)num16[i] - 55) * pow((double)16, size - i - 1);
+		else
+			total += ((int)num16[i] - 48) * pow((double)16, size - i - 1);
+	}
+
+	printf("%d", total);
+
+	return 0;
+}
